37b672825776e81dab7aedcc32684a0f5ac47b2f
Uuna Saarela <uuna.saarela@gmail.com>
2023-08-19 19:13:30 +0300

    3.8

--- phonebook/index.js
+++ phonebook/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('post-body', request => { return JSON.stringify(request.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 
 let persons = [
     { 
