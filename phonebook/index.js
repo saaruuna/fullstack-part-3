78ea03385f2cd05676abacf1a543537f28468848
Uuna Saarela <uuna.saarela@gmail.com>
2023-08-19 17:35:05 +0300

    3.5

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
     { 
@@ -49,6 +50,31 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+const generateId = () => {
+  const newId = Math.floor(Math.random() * persons.length * 100)
+  return newId
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  if (!body.name | !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
