a195ead6243ed2bae827a1ab2d9de4d2a85dcc7f
Uuna Saarela <uuna.saarela@gmail.com>
2023-08-19 17:55:12 +0300

    3.6

--- phonebook/index.js
+++ phonebook/index.js
@@ -58,11 +58,22 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  console.log(body)
 
-  if (!body.name | !body.number) {
+  if (!body.name) {
     return response.status(400).json({ 
-      error: 'content missing' 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  if (persons.some((person) => person.name.includes(body.name))) {
+    return response.status(400).json({
+        error: "name must be unique"
     })
   }
 
