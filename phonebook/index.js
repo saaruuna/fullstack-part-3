8ae41d3c6ad775dd7c8cef9bf829ff8b1bb3043b
Uuna Saarela <uuna.saarela@gmail.com>
2023-08-19 17:14:19 +0300

    3.3

--- phonebook/index.js
+++ phonebook/index.js
@@ -28,8 +28,20 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook contains info for ${persons.length} people</p>
                     <p>${new Date()}</p>`)
 })
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
