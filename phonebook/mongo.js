3edef7924e4061cd9f6e0064e651c4f9aa96e944
Uuna Saarela <uuna.saarela@gmail.com>
2023-08-20 12:47:14 +0300

    3.12

---
+++ phonebook/mongo.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://phonebook:${password}@cluster0.zwdollw.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+      result.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
+} else if (process.argv.length === 5) {
+    const newName = process.argv[3]
+    const newNumber = process.argv[4]
+
+
+    const person = new Person({
+      name: newName,
+      number: newNumber,
+    })
+
+    person.save().then(result => {
+      console.log(`added ${newName} number ${newNumber} to phonebook`)
+      mongoose.connection.close()
+    })
+} else {
+  console.log('usage: node mongo.js <password> <name> <number>')
+  process.exit(1)
+}
+
+
+
+
